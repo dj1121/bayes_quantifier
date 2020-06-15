683d1235c1324eee5f5f424c51f5621a4c167521
Devin Johnson <devinj1121@gmail.com>
2020-06-15 11:26:15 -0400

    update

--- src/experiment.py
+++ src/experiment.py
@@ -1,15 +1,18 @@
 # Main driver file for the program. Set up experiments and run them from here.
 import argparse
+import grammars
+import data_handling
 
-def run_experiment(model, grammar, data_dir, out_dir):
+
+def run_experiment(model, grammar_path, data_dir, out_dir):
     print()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-model', help='which model to run', type=str, required=True)
-    parser.add_argument('-grammar', help='which grammar file to use', type=str, required=True)
+    parser.add_argument('-grammar_path', help='specify a grammar type to generate', type=str, required=True)
     parser.add_argument('-data_dir', help='directory of data for model', default="./../data", type=str, required=True)
     parser.add_argument('-out_dir', help='path to output', type=str, default='./tmp')
     args = parser.parse_args()
 
-    run_experiment(args.model, args.grammar, args.data_dir, args.out_dir)
+    run_experiment(args.model, args.grammar_path, args.data_dir, args.out_dir)
--- src/tools/grammar_handling.py
+++
@@ -1,1 +0,0 @@
-# Set of functions for loading and handling LOT grammars
